b8761d29e1f999edaca59f896280a97d26f9eb9c
Marcos Cavalcanti <24304834+mcavl@users.noreply.github.com>
2023-11-12 14:37:44 -0800

    feature: add clinic_id to patients and practitioners

--- db/migrate/20231112223211_create_practitioners.rb
+++ db/migrate/20231112223211_create_practitioners.rb
@@ -5,6 +5,7 @@ class CreatePractitioners < ActiveRecord::Migration[7.1]
       t.string :last_name
       t.string :specialty
 
+      t.references :clinic, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20231112223247_create_patients.rb
+++ db/migrate/20231112223247_create_patients.rb
@@ -4,6 +4,7 @@ class CreatePatients < ActiveRecord::Migration[7.1]
       t.string :first_name
       t.string :last_name
 
+      t.references :clinic, null: false, foreign_key: true
       t.timestamps
     end
   end
