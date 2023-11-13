aa2fa4bfd85cc8922c30c05d2c56f73d3b722c50
Marcos Cavalcanti <24304834+mcavl@users.noreply.github.com>
2023-11-12 17:43:02 -0800

    chore: add patient annotations

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: patients
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  clinic_id  :integer          not null
+#
+# Indexes
+#
+#  index_patients_on_clinic_id  (clinic_id)
+#
+# Foreign Keys
+#
+#  clinic_id  (clinic_id => clinics.id)
+#
 FactoryBot.define do
   factory :patient do
     first_name { "MyString" }
