bc38ffbfd6e2c9aa8f1a28c56f296d1f497de8f9
Marcos Cavalcanti <24304834+mcavl@users.noreply.github.com>
2023-11-13 13:11:54 -0800

    chore: remove old config value

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,6 @@ module ClinicApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.enable_dependency_loading = true
     config.autoload_paths += %W[#{config.root}/app/services]
     config.autoload_paths += %W[#{config.root}/lib]
   end
