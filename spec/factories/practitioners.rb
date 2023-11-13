5eca19c46c8e5badcda056f43923c380011d6e88
Marcos Cavalcanti <24304834+mcavl@users.noreply.github.com>
2023-11-12 17:44:53 -0800

    chore: add practitioner annotations

--- spec/factories/practitioners.rb
+++ spec/factories/practitioners.rb
@@ -1,7 +1,25 @@
+# == Schema Information
+#
+# Table name: practitioners
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  specialty  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  clinic_id  :integer          not null
+#
+# Indexes
+#
+#  index_practitioners_on_clinic_id  (clinic_id)
+#
+# Foreign Keys
+#
+#  clinic_id  (clinic_id => clinics.id)
+#
 FactoryBot.define do
   factory :practitioner do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    specialty { "MyString" }
+
   end
 end
--- spec/models/practitioner_spec.rb
+++ spec/models/practitioner_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: practitioners
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  specialty  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  clinic_id  :integer          not null
+#
+# Indexes
+#
+#  index_practitioners_on_clinic_id  (clinic_id)
+#
+# Foreign Keys
+#
+#  clinic_id  (clinic_id => clinics.id)
+#
 require 'rails_helper'
 
 RSpec.describe Practitioner, type: :model do
