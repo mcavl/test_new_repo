86589a89d34b659557ed3e2120b30e5d93112e23
Marcos Cavalcanti <24304834+mcavl@users.noreply.github.com>
2023-11-14 21:05:35 -0800

    chore: udpate Gemfile

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: clinics
@@ -18,14 +20,60 @@ class Clinic < ApplicationRecord
   validates :name, :close_time, :open_time, :timezone, presence: true
 
   def closing_time(time = nil)
-    time = Time.now if time.nil?
+    time = current_time if time.nil?
 
-    TimeUtils.time_from_timezone(timezone, "#{time.to_date.to_s} #{close_time}")
+    TimeUtils.time_from_timezone(timezone, "#{time.to_date} #{close_time}")
   end
 
   def opening_time(time = nil)
-    time = Time.now if time.nil?
+    time = current_time if time.nil?
 
-    TimeUtils.time_from_timezone(timezone, "#{time.to_date.to_s} #{open_time}")
+    TimeUtils.time_from_timezone(timezone, "#{time.to_date} #{open_time}")
+  end
+
+  def current_time
+    Time.now.in_time_zone(timezone)
+  end
+
+  def next_available_start_time
+    now = current_time
+    hour, min = next_valid_time(now)
+    Time.new(now.year, now.month, now.day, hour, min, 0, ActiveSupport::TimeZone[timezone])
+  end
+
+  private
+
+  def next_valid_time(now)
+    hour = now.min <= 30 ? now.hour + 2 : now.hour + 3
+    min = next_valid_minute(now)
+    if hour < opening_hour
+      return [opening_hour, opening_minute]
+    elsif hour >= closing_hour
+      return [closing_hour, closing_minute]
+    end
+
+    [hour, min]
+  end
+
+  def opening_hour
+    open_time.split(':').first.to_i
+  end
+
+  def opening_minute
+    open_time.split(':').second.to_i
+  end
+
+  def closing_hour
+    close_time.split(':').first.to_i
+  end
+
+  def closing_minute
+    close_time.split(':').second.to_i
+  end
+
+  def next_valid_minute(now)
+    return 0 if now.min.zero?
+
+    now.min <= 30 ? 30 : 0
   end
 end
--- spec/models/clinic_spec.rb
+++ spec/models/clinic_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: clinics
@@ -13,5 +15,62 @@
 require 'rails_helper'
 
 RSpec.describe Clinic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:gmt_offset) { '-3' }
+  let(:clinic) do
+    FactoryBot.create(:clinic, name: 'Physio Jane', open_time: '09:00',
+                      close_time: '17:00', timezone: TimeUtils.tz(gmt_offset))
+  end
+
+  describe 'validations' do
+    it 'does not create Clinic when missing data' do
+      clinic = Clinic.build(name: nil, close_time: nil, open_time: nil, timezone: nil)
+      expect(clinic.valid?).to be(false)
+      expect(clinic.errors[:name]).to eq(["can't be blank"])
+      expect(clinic.errors[:close_time]).to eq(["can't be blank"])
+      expect(clinic.errors[:open_time]).to eq(["can't be blank"])
+      expect(clinic.errors[:timezone]).to eq(["can't be blank"])
+    end
+  end
+
+  describe 'when calling time methods consider timezone' do
+    it "returns clinic's current time based on timezone" do
+      Timecop.freeze(Time.find_zone(TimeUtils.tz(gmt_offset)).parse('2002-10-30 08:00')) do
+        expect(clinic.current_time.iso8601).to eq('2002-10-30T08:00:00-03:00')
+        expect(clinic.opening_time.iso8601).to eq('2002-10-30T09:00:00-03:00')
+        expect(clinic.closing_time.iso8601).to eq('2002-10-30T17:00:00-03:00')
+      end
+    end
+  end
+
+  describe 'when obtaining next available start time' do
+    it 'returns start time based on current time when minute equals 0' do
+      Timecop.freeze(Time.find_zone(TimeUtils.tz(gmt_offset)).parse('2002-10-30 12:00')) do
+        expect(clinic.next_available_start_time.iso8601).to eq('2002-10-30T14:00:00-03:00')
+      end
+    end
+
+    it 'returns start time based on current time when minute is greater than 0 and less than 30' do
+      Timecop.freeze(Time.find_zone(TimeUtils.tz(gmt_offset)).parse('2002-10-30 09:10')) do
+        expect(clinic.next_available_start_time.iso8601).to eq('2002-10-30T11:30:00-03:00')
+      end
+    end
+
+    it 'returns start time based on current time when minute is greater than 30' do
+      Timecop.freeze(Time.find_zone(TimeUtils.tz(gmt_offset)).parse('2002-10-30 09:34')) do
+        expect(clinic.next_available_start_time.iso8601).to eq('2002-10-30T12:00:00-03:00')
+      end
+    end
+
+    it 'returns start time based on current time when time is before starting time' do
+      Timecop.freeze(Time.find_zone(TimeUtils.tz(gmt_offset)).parse('2002-10-30 03:34')) do
+        expect(clinic.next_available_start_time.iso8601).to eq('2002-10-30T09:00:00-03:00')
+      end
+    end
+
+    it 'returns start time based on current time when time is after closing time' do
+      Timecop.freeze(Time.find_zone(TimeUtils.tz(gmt_offset)).parse('2002-10-30 19:34')) do
+        expect(clinic.next_available_start_time.iso8601).to eq('2002-10-30T17:00:00-03:00')
+      end
+    end
+  end
 end
