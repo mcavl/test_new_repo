3b9f5115b2b67477b792844242b42931df551725
Marcos Cavalcanti <24304834+mcavl@users.noreply.github.com>
2023-11-21 22:50:23 -0800

    refactor: improve BaseService and add some comments

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+##
+# BaseService has general validations that are required for all services
+# As all services require clinic, so results can be correctly scoped,
+# this class validates if clinic_id or clinic was passed.
+# It also has memoized getters to return clinic and clinic_id
+# It has a 'call' class method so the service can be automatically instantiated.
 class BaseService
   def initialize(args)
     @arguments = args
@@ -18,7 +24,7 @@ class BaseService
   private
 
   def self.validate_clinic_arguments(args)
-    raise ::AppointmentService::Errors::ClinicIdMissing, 'Missing clinic id' if args[:clinic_id].nil?
+    raise ::AppointmentService::Errors::ClinicMissing, 'Missing clinic' if args[:clinic_id].nil? && args[:clinic].nil?
   end
 
   attr_reader :arguments
@@ -28,10 +34,16 @@ class BaseService
   end
 
   def find_clinic
+    return arguments.clinic if arguments.respond_to?(:clinic) && arguments.clinic.present?
+
     clinic = Clinic.find_by(id: arguments[:clinic_id])
 
-    raise ::AppointmentService::Errors::ClinicNotFound, 'Clinic not found' if clinic.nil?
+    raise ::AppointmentService::Errors::ClinicNotFound, 'Clinic not found' if clinic.blank?
 
     clinic
+  end
+
+  def clinic_id
+    @clinic_id ||= (arguments[:clinic_id] || clinic.id)
   end
 end
